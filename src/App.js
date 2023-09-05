d6f390b8fcc436972203994c18beb00bb6f518e2
Arham Ramay <arhamramay80@gmail.com>
2023-09-04 23:53:02 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-6xl text-red-500 bg-purple-200'>Tailwind Css Practice</h1>
+      <Hello/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hello.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Hello() {
+  return (
+    <div className='text-6xl text-red-500'>Hello</div>
+  )
+}
+
+export default Hello
